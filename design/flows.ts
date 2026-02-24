162437389fbd8b5276dea361e733e9caaeeab945
Olivier Huin <olih@users.noreply.github.com>
2026-02-24 02:49:12 +0000

    Adds semgrep search

--- design/flows.ts
+++ design/flows.ts
@@ -1,5 +1,5 @@
 import { calls } from './calls';
-import type { ComponentCall, FlowContext } from './common';
+import { type ComponentCall, type FlowContext, incrContext } from './common';
 
 export const cliArgsAnalyse = (context: FlowContext) => {
   const call: ComponentCall = {
@@ -8,6 +8,55 @@ export const cliArgsAnalyse = (context: FlowContext) => {
     directory: 'cmd/maat',
     note: 'flags: --in filename.go --rules io.calls.count,import.file.list --language go --json',
     level: context.level,
+    useCases: [],
+  };
+  calls.push(call);
+};
+
+export const analyseSourceContent = (context: FlowContext) => {
+  const call: ComponentCall = {
+    name: 'file.read',
+    title: 'Read the content of source file and analyse the source file',
+    directory: '',
+    note: '',
+    level: context.level,
+    signature: {
+      input: '{filename, rules, language}',
+    },
+    useCases: [],
+  };
+  calls.push(call);
+  analyseRule(incrContext(context));
+};
+
+export const analyseRule = (context: FlowContext) => {
+  const call: ComponentCall = {
+    name: 'analyse.rule',
+    title: 'Analyse a single rule',
+    directory: '',
+    note: 'Dispatch to right rule analyser than may use different hardcoded approach (ex: semgrep, ...)',
+    level: context.level,
+    signature: {
+      input: '{filename, source, rulename, language}',
+    },
+    useCases: [],
+  };
+  calls.push(call);
+  semgrepSearch(incrContext(context));
+};
+
+export const semgrepSearch = (context: FlowContext) => {
+  const call: ComponentCall = {
+    name: 'semgrep.search',
+    title: 'Search content using a pattern using semgrep',
+    directory:
+      'semgrep requires login to return lines, but we should be able to extract the lines using start/end line/col or possibly offset',
+    note: '',
+    level: context.level,
+    signature: {
+      input: '{source, language, pattern}',
+      success: '{lines}[]',
+    },
     useCases: [],
   };
   calls.push(call);
